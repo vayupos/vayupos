b40012e1e7a7d43991e7284796476ddeb4d84f97
Kavitha04-04Sonachalam <kavithaannakkili@gmail.com>
2026-01-26 19:55:43 +0530

    add notification files

--- backend/app/models/notification.py
+++ backend/app/models/notification.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
-from app.database import Base
+from app.core.database import Base
 
 class Notification(Base):
     __tablename__ = "notifications"
